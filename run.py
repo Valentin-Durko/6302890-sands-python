c041ba36e43302fae19ac78f96fa19aa4ecef09f
Juraj <jpolomsky@tudelft.nl>
2025-09-22 17:27:57 +0200

    this code uses create_sine_wave from signals.py and generates a sine wave with frequency 5 and duration 2 and displays first 10 samples generated by the sine function, fixed z and x label

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ sine = create_sine_wave(5,2)
 print(r'the first 10 samples of my sine wave are',sine[:10])
 
 plt.plot(sine[:10]) 
-plt.ylabel('time')
-plt.xlabel('frequency')
+plt.ylabel('amplitude')
+plt.xlabel('time')
 plt.show
 plt.savefig('sineplot.png')
